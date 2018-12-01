c4565eead94516ffbc420dcf5394c4e64f042dd0
Joseph Mate <mate.joseph@gmail.com>
2018-12-01 10:23:03 -0500

    completed part 2, but couldn't figure out a functional way

--- 01/duplicate_frequency.py
+++ 01/duplicate_frequency.py
@@ -2,13 +2,31 @@ import sys
 from functional import seq
 
 lines = sys.stdin.readlines()
-frequency = (seq(lines)
+preProcessed = (seq(lines)
                 .map(lambda line: line.rstrip())
                 .map(lambda line: line.replace('+', ''))
                 .map(lambda line: int(line))
-                .sum()
              )
 
+frequency = preProcessed.sum()
 print(f'Frequency: {frequency}')
-    
 
+duplicateFrequency = 0;
+frequenciesSeen = {0}
+searching = True
+frequency = 0
+
+while searching:
+    for line in lines:
+        line.rstrip()
+        line.replace('+', '')
+        frequency = frequency + int(line)
+
+        if searching:
+            if frequency in frequenciesSeen:
+                searching = False
+                duplicateFrequency = frequency
+            else:
+                frequenciesSeen.add(frequency)
+
+print(f'Repeat frequency: {duplicateFrequency}')
