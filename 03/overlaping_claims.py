8b9d4151ba63e58637db28ed1603232a80595157
Joseph Mate <mate.joseph@gmail.com>
2018-12-03 22:18:53 -0500

    modified part one to use a set instead of a counter to make part two easier

--- 03/overlaping_claims.py
+++ 03/overlaping_claims.py
@@ -2,9 +2,6 @@ import sys
 import re
 from functional import seq
 
-
-
-
 class Claim:
     def __init__(self, claimId, col, row, colLen, rowLen):
         self.claimId = claimId
@@ -40,36 +37,33 @@ def appendPositionToClaimMap(claimMap, claim, col, row):
     innerMap = claimMap[col]
 
     if row in innerMap:
-        innerMap[row] = innerMap[row] + 1
+        innerMap[row].add(claim.claimId)
     else:
-        innerMap[row] = 1 
+        innerMap[row] = {claim.claimId}
 
 def appendClaimToClaimMap(claimMap, claim):
     for c in range(0, claim.colLen):
         for r in range(0, claim.rowLen):
             appendPositionToClaimMap(claimMap, claim, claim.col + c, claim.row + r)
 
+claims = (seq(sys.stdin)
+                .map(lambda line: line.rstrip())  # remove the newline that readlines keeps
+                .filter(lambda line: claimRegex.match(line))  # remove the newline that readlines keeps
+                .map(lambda line: claimFromEncodedStr(line))     
+             )
+
 claimMap = {}
-lineNum = 1
-for line in sys.stdin:
-    line = line.rstrip()
-
-    if claimRegex.match(line):
-        claim = claimFromEncodedStr(line)
-        appendClaimToClaimMap(claimMap, claim)
-
-        
-    else:
-        print(f'{lineNum}: {line} does not match claim regex')
-
-    lineNum += 1
+for claim in claims:
+    appendClaimToClaimMap(claimMap, claim)
 
 numContestedClaims = 0
 for c in claimMap:
     innerMap = claimMap[c]
     for r in innerMap:
-        if innerMap[r] >= 2:
+        if len(innerMap[r]) >= 2:
             numContestedClaims += 1
 
 print(f'number of inches with two or more claims: {numContestedClaims}')
+
+
 
