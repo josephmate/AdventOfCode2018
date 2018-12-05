929bb4956e28b8c68e947833361c8dcd3c9638e0
Joseph Mate <mate.joseph@gmail.com>
2018-12-04 21:31:22 -0500

    reading and print the sleeping table

--- 04/guard_duty.py
+++ 04/guard_duty.py
@@ -13,35 +13,49 @@ guardLogs.sort()
 
 claimRegex = re.compile(r'#(\d+) @ (\d+),(\d+): (\d+)x(\d+)')
 # [1518-03-18 00:01] Guard #89 begins shift
-gaurdStartRegex = re.compile(r'\[(\d\d\d\d-\d\d-\d\d) (\d\d):(\d\d)\] Guard #(\d+) begins shift')
+gaurdStartRegex = re.compile(r'\[\d\d\d\d-\d\d-\d\d \d\d:\d\d\] Guard #(\d+) begins shift')
 # [1518-03-18 00:35] falls asleep
-gaurdSleepRegex = re.compile(r'\[(\d\d\d\d-\d\d-\d\d) (\d\d):(\d\d)\] falls asleep')
+gaurdSleepRegex = re.compile(r'\[\d\d\d\d-\d\d-\d\d \d\d:(\d\d)\] falls asleep')
 # [1518-03-18 00:59] wakes up
-gaurdWakeRegex = re.compile(r'\[(\d\d\d\d-\d\d-\d\d) (\d\d):(\d\d)\] wakes up')
+gaurdWakeRegex = re.compile(r'\[\d\d\d\d-\d\d-\d\d \d\d:(\d\d)\] wakes up')
 
-class Date:
-    def __init__(self, year, month, day):
-        self.year = year
-        self.month = month
-        self.day = day
-
-    def toString(self):
-        return f'[{self.year}-{self.month}-{self.day}]'
 
 class GuardShift:
     def __init__(self, guardId):
         self.guardId = guardId
+        self.sleeping = []
+        for i in range(0, 60):
+            self.sleeping.append(False)
+
 
 # map of date 
-sleepMap = {} 
-
+sleepMap = [] 
 for guardLog in guardLogs:
     if gaurdStartRegex.match(guardLog):
-        None
+        parsedRecord = gaurdStartRegex.match(guardLog)
+        currentShift = GuardShift(int(parsedRecord.group(1)))
+        sleepMap.append(currentShift)
     elif gaurdSleepRegex.match(guardLog):
-        None
+        parsedRecord = gaurdSleepRegex.match(guardLog)
+        sleepStart = int(parsedRecord.group(1))
     elif gaurdWakeRegex.match(guardLog):
-        None
+        parsedRecord = gaurdWakeRegex.match(guardLog)
+        sleepEnd = int(parsedRecord.group(1))
+        for i in range(sleepStart, sleepEnd):
+            currentShift.sleeping[i] = True
+
     else:
         print('UNRECOGNIZED: ' + guardLog)
 
+counter = 0
+for sleepEntry in sleepMap:
+    sys.stdout.write(f'{counter}\t{sleepEntry.guardId}')
+    sys.stdout.write('\t|')
+    for isSleeping in sleepEntry.sleeping:
+        if isSleeping:
+            sys.stdout.write(' ')
+        else:
+            sys.stdout.write('#')
+    sys.stdout.write('\n')
+    counter+=1
+        
