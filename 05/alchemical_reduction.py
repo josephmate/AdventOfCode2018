d463fdf9841b73f575964cc463f3668a746aacb2
Joseph Mate <mate.joseph@gmail.com>
2018-12-05 20:11:34 -0500

    bug in here somewhere. i think i'm rotating wrong

--- 05/alchemical_reduction.py
+++ 05/alchemical_reduction.py
@@ -11,19 +11,77 @@ def willRecact(a, b):
             )
 
 chars = list(sys.stdin.readline().rstrip())
-doublyLinkedList = deque()
+# tried using list in my previous soltuion and then it was taking forever
+# tried using a moving index, since we already know characters before the index will not react
+# but that turned out to be really complicated.
+# tried searching for a doubly linked list in python and found a deque.
+# same thing as a doubly linked list, but instead we have this 'rotate' method for moving
+# the list forward when we do not see a reaction
+# our 'pointer' is the left most element
+dq = deque()
 
+dq.append("FIRST") # prevents us from rotating to the 
+for c in chars:
+    # message
+    # append m to right most
+    # m
+    # append e to right most
+    # me
+    # append s to right most
+    # mes
+    # ... etc
+    dq.append(c)
+dq.append("END")
+#   |
+#   V
+# FIRST message END
+dq.rotate(-1)
+# |
+# V
+# message END FIRST 
+print(dq)
 
-reactionHappen = True
-while reactionHappen:
-    reactionHappened = False
-    for i in range(0, len(chars)):
-        if i + 1 < len(chars):
-            if willRecact(chars[i], chars[i+1]):
-                #print(f'{chars[i]} reacted with {chars[i+1]}')
-                del chars[i:i+1]
-                reactionHappened = True
-                break
+while True:
+    print(dq)
+    # arrived at the end, nothing left to read
+    currentChar = dq.popleft()
+    if currentChar == "END":
+        break;
 
-print(str(chars))
-print(len(chars))
+    # arrived at the end, nothing left to read,
+    # but need to put back the previous character
+    nextChar = dq.popleft()
+    if nextChar == "END":
+        dq.appendleft(currentChar)
+        break;
+
+    if willRecact(currentChar, nextChar):
+        print(f'{currentChar} reacted with {nextChar}')
+        # do not put the characters back
+        # need to rotate to left once because maybe the previous character might react
+        # example
+        #  VV
+        # abBA
+        #  VV
+        # aA
+        # rotating back one
+        # vv
+        # aA
+        dq.rotate(-1)
+        checkFirst = dq.popleft()
+        dq.appendleft(checkFirst)
+        # make sure we're not at the "FIRST"
+        if checkFirst == "FIRST":
+            dq.rotate(1)
+    # a reaction did not occur so we can advance the 'pointer'
+    else:
+        # VV
+        # message END FIRST
+        # VV
+        # essage END FIRST m
+        dq.appendleft(nextChar)
+        dq.appendleft(currentChar)
+        dq.rotate(-1)
+
+print(dq)
+print(len(dq) - 1) # -1 because FIRST is still in the list
