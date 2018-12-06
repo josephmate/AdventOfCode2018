9750e93c731450970f4d28bcb7d4b8178ff09bda
Joseph Mate <mate.joseph@gmail.com>
2018-12-05 19:40:37 -0500

    tried doing inefficient solution but too slow. checkpointing then optimizing

--- 05/alchemical_reduction.py
+++ 05/alchemical_reduction.py
@@ -1,9 +1,29 @@
 import sys
 import re
 from functional import seq
+from collections import deque
 
-
+def willRecact(a, b):
+    #print(f'{a} != {b}: {a != b}')
+    #print(f'{a.upper()} == {b.upper()}: {a.upper() == b.upper()}')
+    return ( a != b
+            and a.upper() == b.upper()
+            )
 
 chars = list(sys.stdin.readline().rstrip())
+doublyLinkedList = deque()
 
-print(chars)
+
+reactionHappen = True
+while reactionHappen:
+    reactionHappened = False
+    for i in range(0, len(chars)):
+        if i + 1 < len(chars):
+            if willRecact(chars[i], chars[i+1]):
+                #print(f'{chars[i]} reacted with {chars[i+1]}')
+                del chars[i:i+1]
+                reactionHappened = True
+                break
+
+print(str(chars))
+print(len(chars))
