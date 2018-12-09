6778287e19df836fcffa336227325d35afbc68be
Joseph Mate <mate.joseph@gmail.com>
2018-12-08 20:31:53 -0500

    part 1 problem 06 got rid of dead code

--- 06/chronal_coordinates.py
+++ 06/chronal_coordinates.py
@@ -13,60 +13,6 @@ class Point:
     def __repr__(self):
         return f'{self.x, self.y}'
 
-    def isAbove(self, other):
-        return self.y > other.y
-
-    def isBelow(self, other):
-        return self.y < other.y
-
-    def isLeftOf(self, other):
-        return self.x < other.x
-
-    def isRightOf(self, other):
-        return self.x > other.x
-
-    def isFinite(self, points):
-        isSomethingAbove = False
-        isSomethingBelow = False
-        isSomethingLeft = False
-        isSomethingRight = False
-
-        for other in points:
-            isSomethingBelow = isSomethingBelow or self.isAbove(other)
-            isSomethingAbove = isSomethingAbove or self.isBelow(other)
-            isSomethingRight = isSomethingRight or self.isLeftOf(other)
-            isSomethingLeft = isSomethingLeft or self.isRightOf(other)
-
-        return isSomethingBelow and isSomethingAbove and isSomethingRight and isSomethingLeft
-
-    def closestAbove(self, points):
-        abovePoints = (seq(points)
-                        .filter(lambda other: other.isAbove(self))
-                        .list()
-                        )
-        return min(abovePoints, key=lambda other: other.y-self.y)
-
-    def closestBelow(self, points):
-        belowPoints = (seq(points)
-                        .filter(lambda other: other.isBelow(self))
-                        .list()
-                        )
-        return min(belowPoints, key=lambda other: self.y-other.y)
-
-    def closestToLeft(self, points):
-        leftPoints = (seq(points)
-                        .filter(lambda other: other.isLeft(self))
-                        .list()
-                        )
-        return min(leftPoints, key=lambda other: self.x-other.x)
-
-    def closestToRight(self, points):
-        rightPoints = (seq(points)
-                        .filter(lambda other: other.isRight(self))
-                        .list()
-                        )
-        return min(leftPoints, key=lambda other: other.x-self.x)
-
     def manhattanDistance(self, other):
         return abs(self.x - other.x) + abs(self.y - other.y)
 
