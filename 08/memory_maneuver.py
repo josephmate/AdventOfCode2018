1fc166cd1a6e1a21c3a6b72ce17e80d517feb4a5
Joseph Mate <mate.joseph@gmail.com>
2018-12-09 12:38:27 -0500

    part 1 of problem 08: unsorted tree

---
+++ 08/memory_maneuver.py
@@ -0,0 +1,61 @@
+import sys
+import re
+from functional import seq
+
+class TreeNode:
+    def __init__(self, children, metadata):
+        self.children = children
+        self.metadata = metadata
+
+    def printTree(self, spacing):
+        moreSpacing = spacing + ' '
+        res = f'{spacing}{self.metadata}'
+        for child in self.children:
+            res += f'\n{spacing}{child.printTree(moreSpacing)}'
+        return res
+
+    def __str__(self):
+        return self.printTree('')
+
+    def __repr__(self):
+        return self.printTree('')
+    
+
+    def sum(self):
+        return (seq(self.metadata).sum()
+                    + seq(self.children).map(lambda subTree: subTree.sum()).sum()
+                )
+    
+
+
+def parseTreeSpec(treeSpec, index):
+    numOfChildren = treeSpec[index];
+    index += 1;
+    numOfMetaData = treeSpec[index];
+    index += 1;
+    
+    children = []
+    for i in range(0, numOfChildren):
+        (subTree, index) = parseTreeSpec(treeSpec, index)
+        children.append(subTree)
+    
+    metadata = []
+    for i in range(0, numOfMetaData):
+        metadata.append(treeSpec[index])
+        index += 1;
+
+    return TreeNode(children, metadata), index
+
+
+treeSpec = (seq(sys.stdin.readline().rstrip().split(' '))
+                .map(lambda string: int(string))
+                .list()
+           )
+
+
+
+print(treeSpec)
+
+(tree, indexesConsumed) = parseTreeSpec(treeSpec, 0)
+print(tree)
+print(tree.sum())
