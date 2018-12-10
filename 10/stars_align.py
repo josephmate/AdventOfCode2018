b67052f60e70d501c5e67b8b1551539428627449
Joseph Mate <mate.joseph@gmail.com>
2018-12-10 08:36:32 -0500

    done problem 10
    I thought max adjacency would get me the answer, but I still had to manually inspect the solutions close t the max adjacency

--- 10/stars_align.py
+++ 10/stars_align.py
@@ -14,6 +14,13 @@ class Star:
     def __repr__(self):
         return f'[{self.posn} {self.velocity}]'
 
+    def move(self):
+        return Star(Point(self.posn.x + self.velocity.x,
+                          self.posn.y + self.velocity.y
+                         ),
+                    Point(self.velocity.x, self.velocity.y)
+                   )
+
 class Point:
     def __init__(self, x, y):
         self.x = x
@@ -24,6 +31,12 @@ class Point:
 
     def __repr__(self):
         return f'({self.x} {self.y})'
+
+    def __hash__(self):
+        return hash((self.x, self.y))
+
+    def __eq__(self, other):
+        return (self.x, self.y) == (other.x, other.y)
 
 starRegex = re.compile(r'position=< *(-?\d+), *(-?\d+)> velocity=< *(-?\d+), *(-?\d+)>')
 def starFromStr(starStr):
@@ -40,5 +53,69 @@ for line in sys.stdin:
     else:
         print(f'ERROR {lineNum}: {line}')
 
-print(stars)
-print(len(stars))
+# count how many points are next to other points
+def calcAdjacency(stars):
+    points = set()
+    for star in stars:
+        points.add(star.posn)
+
+    adjacency = 0
+    # we iterate over points instead of stars because some stars might be ontop
+    # of eachother and so we should reduce the adjacency count in that case
+    for point in points: 
+        if Point(point.x+1, point.y) in points: adjacency += 1
+        if Point(point.x-1, point.y) in points: adjacency += 1
+        if Point(point.x, point.y+1) in points: adjacency += 1
+        if Point(point.x, point.y-1) in points: adjacency += 1
+        if Point(point.x+1, point.y+1) in points: adjacency += 1
+        if Point(point.x-1, point.y+1) in points: adjacency += 1
+        if Point(point.x+1, point.y-1) in points: adjacency += 1
+        if Point(point.x-1, point.y-1) in points: adjacency += 1
+
+    return adjacency
+
+
+def moveStars(stars):
+    newStar = []
+    for star in stars:
+        newStar.append(star.move())
+    return newStar
+
+def printStars(stars):
+    minX = min(seq(stars).map(lambda star: star.posn.x))
+    maxX = max(seq(stars).map(lambda star: star.posn.x))
+    minY = min(seq(stars).map(lambda star: star.posn.y))
+    maxY = max(seq(stars).map(lambda star: star.posn.y))
+
+    points = set()
+    for star in stars:
+        points.add(star.posn)
+    
+    for y in range (minY, maxY+1):
+        for x in range (minX, maxX+1):
+            if Point(x, y) in points:
+                print('#', end='')
+            else:
+                print(' ', end='')
+        print('\n', end='')
+
+
+
+starIterator = stars
+maxAdjacency = 0
+for i in range(0, 15000):
+    adjacency = calcAdjacency(starIterator)
+    if adjacency > maxAdjacency:
+        maxAdjacency = adjacency
+        whenMaxAdjacency = i
+    starIterator = moveStars(starIterator)
+    
+
+starIterator = stars
+for i in range(0, whenMaxAdjacency+50):
+    if i > whenMaxAdjacency - 50 and  i < whenMaxAdjacency + 50:
+        printStars(starIterator)
+        print(i)
+        print(calcAdjacency(starIterator))
+        print('==========================')
+    starIterator = moveStars(starIterator)
