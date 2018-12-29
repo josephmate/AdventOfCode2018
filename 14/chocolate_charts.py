72e55a22478708428b49e6b42191802589c00e53
Joseph Mate <mate.joseph@gmail.com>
2018-12-29 16:48:47 -0500

    solved part 2. forgot that exit condition could be satisfied inbetween when two recipes are added
    discovered this by extrapolating from the test cases given to me

--- 14/chocolate_charts.py
+++ 14/chocolate_charts.py
@@ -48,6 +48,14 @@ print(f'solve1(18) = {solve1(18)} should be: 9251071085')
 print(f'solve1(2018) = {solve1(2018)} should be: 5941429882')
 print(f'solve1(540391) = {solve1(540391)}')
 
+def checkCondition(val, scores):
+    condition = ""
+    if len(scores)-len(val) >= 0:
+        for i in range(len(scores)-len(val), len(scores)):
+            condition += str(scores[i])
+        
+        #print(condition)
+    return condition == val
 
 def solve2(val):
     #Only two recipes are on the board: the first recipe got a score of 3, the second, 7.
@@ -71,7 +79,11 @@ def solve2(val):
         recipeSum = scores[elf1Pointer] + scores[elf2Pointer]
         if recipeSum >= 10:
             scores.append(recipeSum // 10)
+            if checkCondition(val, scores):
+                return len(scores) - len(val)
         scores.append(recipeSum % 10)
+        if checkCondition(val, scores):
+            return len(scores) - len(val)
 
         # each Elf picks a new current recipe. To do this, the Elf steps forward
         # through the scoreboard a number of recipes equal to 1 plus the score of
@@ -82,41 +94,33 @@ def solve2(val):
         elf1Pointer = (elf1Pointer + scores[elf1Pointer] + 1) % len(scores)
         elf2Pointer = (elf2Pointer + scores[elf2Pointer] + 1) % len(scores)
 
-        if len(scores)-len(val) >= 0:
-            condition = ""
-            for i in range(len(scores)-len(val), len(scores)):
-                condition += str(scores[i])
-
-            if condition == val:
-                return len(scores) - len(val)
 
 
 
+
+print(f'solve2("51589") = {solve2("51589")} should be: 9')
+print(f'solve2("515891") = {solve2("515891")} should be: 9')
+print(f'solve2("5158916") = {solve2("5158916")} should be: 9')
+print(f'solve2("51589167") = {solve2("51589167")} should be: 9')
+print(f'solve2("515891677") = {solve2("515891677")} should be: 9')
+print(f'solve2("5158916779") = {solve2("5158916779")} should be: 9')
+print(f'solve2("01245") = {solve2("01245")} should be: 5')
+print(f'solve2("012451") = {solve2("012451")} should be: 5')
+print(f'solve2("0124515") = {solve2("0124515")} should be: 5')
+print(f'solve2("01245158") = {solve2("01245158")} should be: 5')
+print(f'solve2("012451589") = {solve2("012451589")} should be: 5')
 print(f'solve2("0124515891") = {solve2("0124515891")} should be: 5')
-
-if False:
-    print(f'solve2("51589") = {solve2("51589")} should be: 9')
-    print(f'solve2("515891") = {solve2("515891")} should be: 9')
-    print(f'solve2("5158916") = {solve2("5158916")} should be: 9')
-    print(f'solve2("51589167") = {solve2("51589167")} should be: 9')
-    print(f'solve2("515891677") = {solve2("515891677")} should be: 9')
-    print(f'solve2("5158916779") = {solve2("5158916779")} should be: 9')
-    print(f'solve2("01245") = {solve2("01245")} should be: 5')
-    print(f'solve2("012451") = {solve2("012451")} should be: 5')
-    print(f'solve2("0124515") = {solve2("0124515")} should be: 5')
-    print(f'solve2("01245158") = {solve2("01245158")} should be: 5')
-    print(f'solve2("012451589") = {solve2("012451589")} should be: 5')
-    print(f'solve2("92510") = {solve2("92510")} should be: 18')
-    print(f'sovle2("925107") = {solve2("925107")} should be: 18')
-    print(f'sovle2("9251071") = {solve2("9251071")} should be: 18')
-    print(f'sovle2("92510710") = {solve2("92510710")} should be: 18')
-    print(f'sovle2("925107108") = {solve2("925107108")} should be: 18')
-    print(f'sovle2("9251071085") = {solve2("9251071085")} should be: 18')
-    print(f'solve2("59414") = {solve2("59414")} should be: 2018')
-    print(f'solve2("594142") = {solve2("594142")} should be: 2018')
-    print(f'solve2("5941429") = {solve2("5941429")} should be: 2018')
-    print(f'solve2("59414298") = {solve2("59414298")} should be: 2018')
-    print(f'solve2("594142988") = {solve2("594142988")} should be: 2018')
-    print(f'solve2("5941429882") = {solve2("5941429882")} should be: 2018')
-    print(f'solve2("540391") = {solve2("540391")}')
+print(f'solve2("92510") = {solve2("92510")} should be: 18')
+print(f'sovle2("925107") = {solve2("925107")} should be: 18')
+print(f'sovle2("9251071") = {solve2("9251071")} should be: 18')
+print(f'sovle2("92510710") = {solve2("92510710")} should be: 18')
+print(f'sovle2("925107108") = {solve2("925107108")} should be: 18')
+print(f'sovle2("9251071085") = {solve2("9251071085")} should be: 18')
+print(f'solve2("59414") = {solve2("59414")} should be: 2018')
+print(f'solve2("594142") = {solve2("594142")} should be: 2018')
+print(f'solve2("5941429") = {solve2("5941429")} should be: 2018')
+print(f'solve2("59414298") = {solve2("59414298")} should be: 2018')
+print(f'solve2("594142988") = {solve2("594142988")} should be: 2018')
+print(f'solve2("5941429882") = {solve2("5941429882")} should be: 2018')
+print(f'solve2("540391") = {solve2("540391")}')
 
